79f9b7dd3ce33cc7a4c7c747bdb3ff5e26c5cb57
Mica-P <74531804+Mica-P@users.noreply.github.com>
2021-03-30 11:27:52 +0200

    Slideshow done bigscreen

--- Energie-drank/energiedrankWebsite/producten.js
+++ Energie-drank/energiedrankWebsite/producten.js
@@ -1,22 +1,27 @@
-var dice; //Hier komt de locatie van de Div dice in te staan
-var nummers; //Hier komt de locatie van de Div Nummers in te staan
-var rollDice; //Hier komt de locatie van de button rollDice in te staan
+var images;
+var button;
+var index = 0;
+var productImage = ['orange', 'lemon-tea', 'cherry', 'grape'];
 
-var slideShow = ["orange.gif", "lemon-tea.gif", "cherry.gif", "grape.gif"];
-
-var arrayNummers = [''];
+var slideShowArray = ['orange', 'lemon-tea', 'cherry', 'grape'];
 
 
 
 window.onload = function () {
-    dice = document.querySelector(".dice");
-    nummers = document.querySelector(".nummers");
-    rollDice = document.querySelector(".rollDice");
+    test = document.getElementById("images");
+    setInterval(slideShow, 1200);
 
-    rollDice.onclick = function () {
-        randNummer = Math.floor(Math.random() * 5);
-        dice.style.backgroundImage = "url(images/" + slideShow[randNummer] + ")";
-
+}
+function slideShow() {
+    test.style.backgroundImage = "url(images/" + slideShowArray[index] + ".gif)";
+    index++;
+    if (index == slideShowArray.length) {
+        index = 0;
     }
 }
 
+function getArrayForEach() {
+    for (var product of productImage) {
+        test.innerHTML += product + "<br>";
+    }
+}
