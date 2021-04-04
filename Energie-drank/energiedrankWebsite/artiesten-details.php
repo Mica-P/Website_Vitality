edd48d14f8c8eeb73579281cc97327618f7ac6a3
Mike van Duyvenbode <mikeduyvenbode070@hotmail.com>
2021-04-04 19:15:57 +0200

    verkrachting van een wqilde beer

--- Energie-drank/energiedrankWebsite/artiesten-details.php
+++ Energie-drank/energiedrankWebsite/artiesten-details.php
@@ -8,10 +8,10 @@
     <meta name="keywords" content="Vitality, energiedrank, website">
     <meta name="author" content="Mica Paulo, Sijme Heemskerk, Mike van Duyvenbode, Dinand Dummer">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="./css/artiesten-details.css">
+    <link rel="stylesheet" type="text/css" href="./css/aanbieding-details.css">
     <link rel="shortcut icon" href="images/mini-logo.gif">
 
-    <title>artietsen details</title>
+    <title>Aanbiedingen details1</title>
 </head>
 <?php
 //stap 1
