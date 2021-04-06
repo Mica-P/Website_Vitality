a6d3c8ae6db7fb13f5bfb20d534e596cbeffa553
Mica-P <74531804+Mica-P@users.noreply.github.com>
2021-04-06 10:37:24 +0200

    done with goede doelen pagina + smallscreen

--- Energie-drank/energiedrankWebsite/goededoelen.js
+++ Energie-drank/energiedrankWebsite/goededoelen.js
@@ -3,17 +3,18 @@ var button;
 var index = 0;
 var productImage = ['goededoel1', 'goededoel2', 'goededoel3', 'goededoel4'];
 
-var slideShowArray = ['goededoel1', 'goededoel2', 'goededoel3', 'goededoel4'];
+var slideShowArray = ['goededoel1.jpg', 'goededoel2.png', 'goededoel3.png', 'goededoel4.jpg'];
 
 
 
 window.onload = function () {
     test = document.getElementById("images");
     setInterval(slideShow, 1200);
+    test.style.height = "300";
 
 }
 function slideShow() {
-    test.style.backgroundImage = "url(images/" + slideShowArray[index] + ".jpg)";
+    test.style.backgroundImage = "url(images/" + slideShowArray[index] + ")";
     index++;
     if (index == slideShowArray.length) {
         index = 0;
